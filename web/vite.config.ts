a5ef280ed93de216cd5e4fc149a7a99349c648f4
Raju Khunt <rajukhunt15@gmail.com>
2024-03-28 13:37:50 +0530

    Remove process define

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": {
-      REACT_APP_API_ENDPOINT: null,
-    },
-  },
 });
